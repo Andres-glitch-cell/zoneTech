75766c0fe9037c69188046d02f9ce37564bfcef3
Andrés <afernandezs.iesjc@gmail.com>
2025-12-25 00:30:26 +0100

    Apartado de comentarios en web.php

--- zoneTechLaravel/routes/web.php
+++ zoneTechLaravel/routes/web.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * 🚀 GUÍA DE GIT - TRABAJO EN EQUIPO
+ * --------------------------------
+ * 1. ACTUALIZAR (Pull):
+ * git checkout main
+ * git pull origin main
+ * git checkout Andres
+ * git merge main
+ *
+ * 2. GUARDAR (Commit):
+ * git add .
+ * git commit -m "Descripción de los cambios"
+ *
+ * 3. SUBIR (Push):
+ * git push origin Andres
+ *
+ * 4. FIX .GITIGNORE (Si falla):
+ * git rm -r --cached .
+ * git add .
+ * git commit -m "Limpieza de cache"
+ */
 
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +31,6 @@ Route::get('/', function () {
 ! Texto de bienvenida personalizado con fn() => 
  route::get("/", fn() => "<h1>¡Hola! ZoneTech está online 🚀</h1>");
  */
+
+
+?>
