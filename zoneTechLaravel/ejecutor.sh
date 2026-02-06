fe2ab0e63e1514c862344ffff8fe41e48f9ae53f
Andrés Fernández Salaud <afernandezs.iesjc@gmail.com>
2026-02-06 18:43:43 +0000

    Cambio de extensión de .log a .txt

--- zoneTechLaravel/ejecutor.sh
+++ zoneTechLaravel/ejecutor.sh
@@ -8,4 +8,4 @@ MENSAJE="Script Ejecutado Correctamente ${ACTUAL_DATE} ${ACTUAL_TIME}"
 echo "$MENSAJE"
 # 2. Guardar en un archivo de log (opcional pero recomendado para seguimiento)
 # Esto se guardará en la misma carpeta donde esté el script
-echo "$MENSAJE" >> "$(dirname "$0")/registro_ejecucion.log"
+echo "$MENSAJE" >> "$(dirname "$0")/registro_ejecucion.txt"
