d73c699b783b8089444a9870f7eae6c446bafe4f
Andrés Fernández Salaud <afernandezs.iesjc@gmail.com>
2026-02-06 18:35:45 +0000

    Mostrar archivos .log

--- zoneTechLaravel/ejecutor.sh
+++ zoneTechLaravel/ejecutor.sh
@@ -4,10 +4,8 @@ ACTUAL_DATE=$(date +"%Y-%m-%d")
 ACTUAL_TIME=$(date +"%H:%M:%S")
 # Mensaje a enviar/mostrar
 MENSAJE="Script Ejecutado Correctamente ${ACTUAL_DATE} ${ACTUAL_TIME}"
-
 # 1. Imprimir en consola
 echo "$MENSAJE"
-
 # 2. Guardar en un archivo de log (opcional pero recomendado para seguimiento)
 # Esto se guardará en la misma carpeta donde esté el script
 echo "$MENSAJE" >> "$(dirname "$0")/registro_ejecucion.log"
