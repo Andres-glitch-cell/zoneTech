50f0b20d439cd6d24ed0ecdc09be962fe04e1d93
Andrés Fernández Salaud <afernandezs.iesjc@gmail.com>
2026-02-15 18:46:17 +0000

    Cambios .sql

--- zoneTech.sql
+++ zoneTech.sql
@@ -1,18 +1,18 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1deb3
--- https://www.phpmyadmin.net/
+-- https://www.phpmyadmin.net/dd
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 06-02-2026 a las 15:43:54
+-- Tiempo de generación: 15-02-2026 a las 18:18:04
 -- Versión del servidor: 8.0.45-0ubuntu0.24.04.1
--- Versión de PHP: 8.4.17
+-- Versión de PHP: 8.4.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+dd
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLiit cIENT=@@CHARACTdER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
@@ -164,38 +164,57 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('Dich5PUp0NUQAJ7lNbXDlJ6L4cynhvsMQileCnCj', NULL, '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiY0w1S2RoMmhCYXliYjJOTk5jMm1SYTVSaTJKT3ZRaGY2Tk5TQjFUWiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9yZWdpc3RlciI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1770392202);
+('5FsdXJSlaazs1uWknpBEODJXtUo7TrPk9BAATqnA', 4, '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiS2FqRHZTYmtLS2ZHT2ZsR2xuRFFxQk9BM1cyMEZUamk3a2tVdUVERSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9zb3BvcnRlLXRlY25pY28iO3M6NToicm91dGUiO3M6Nzoic29wb3J0ZSI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjQ7fQ==', 1771179079);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `usuariosAutenticados`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE `usuariosAutenticados` (
   `id` bigint UNSIGNED NOT NULL,
-  `dni` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `apellido1` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
   `apellido2` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `pais` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ciudad` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `poblacion` varchar(110) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `codigoPostal` mediumint NOT NULL,
-  `direccion` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `contraseña_hash` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `rol` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'cliente'
+  `rol` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'usuario',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuariosNoAutenticados`
+--
+
+CREATE TABLE `usuariosNoAutenticados` (
+  `id` bigint UNSIGNED NOT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido1` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido2` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `iniciales` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contraseña_hash` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rol` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'usuario',
+  `token_verificacion` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_modificacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `usuariosNoAutenticados`
 --
 
-INSERT INTO `usuarios` (`id`, `dni`, `nombre`, `apellido1`, `apellido2`, `pais`, `ciudad`, `poblacion`, `codigoPostal`, `direccion`, `email`, `telefono`, `contraseña_hash`, `fecha_nacimiento`, `rol`) VALUES
-(1, '12345678', 'andres', 'fernandez', 'salaud', 'españa', 'vinaros', 'castellon', 12500, 'direccion', 'afernandez@gmail.com', '666666666', 'sdlkfjadslkj', NULL, 'cliente');
+INSERT INTO `usuariosNoAutenticados` (`id`, `usuario`, `nombre`, `apellido1`, `apellido2`, `iniciales`, `email`, `contraseña_hash`, `rol`, `token_verificacion`, `fecha_creacion`, `fecha_modificacion`, `created_at`, `updated_at`) VALUES
+(2, 'noaheljugador', 'Noah', 'Mertens', 'Solans', NULL, 'noahmertens@gmail.com', '$2y$12$to.K8UdIrLyOwUdSa9B4xuH0b0fUfl6TeKYnYB630OD46LEx0f.OS', 'usuario', NULL, '2026-02-15 15:59:12', '2026-02-15 15:59:12', NULL, NULL),
+(3, 'andreslol', 'andres', 'fernandez', 'salaud', 'AF', 'andres@gmail.com', '$2y$12$n4yBQ2fUFyspqMv5E3v39eEMi3VR2QEO2KxXaEClgN5BV9429YDKW', 'usuario', NULL, '2026-02-15 17:13:51', '2026-02-15 17:13:51', '2026-02-15 17:13:51', '2026-02-15 17:13:51'),
+(4, 'carolinagombau', 'Carolina', 'Gombau', 'Tena', 'CG', 'carolinagombau@gmail.com', '$2y$12$3L8nguIPDfkMHihJJ/dG..V/o8klTYkizajiKTS7MMbb48todPo3u', 'usuario', NULL, '2026-02-15 18:02:56', '2026-02-15 18:02:56', '2026-02-15 18:02:56', '2026-02-15 18:02:56');
 
 --
 -- Índices para tablas volcadas
@@ -260,12 +279,19 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `usuariosAutenticados`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `usuariosAutenticados`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usuarios_dni_unique` (`dni`),
   ADD UNIQUE KEY `usuarios_email_unique` (`email`);
+
+--
+-- Indices de la tabla `usuariosNoAutenticados`
+--
+ALTER TABLE `usuariosNoAutenticados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -296,10 +322,16 @@ ALTER TABLE `productos`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `usuariosAutenticados`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `usuariosAutenticados`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuariosNoAutenticados`
+--
+ALTER TABLE `usuariosNoAutenticados`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
